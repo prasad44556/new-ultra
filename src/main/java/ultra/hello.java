6223fd45659b5ac6e85c8b0b3df4217734e17f22
Nani <Nanigovvala11456@gmail.com>
2018-03-02 09:55:08 +0530

    1st commit

---
+++ src/main/java/ultra/hello.java
@@ -0,0 +1,9 @@
+package ultra;
+
+public class hello {
+	public String Sample(){
+		return "all the files are to be saved";
+		
+	}
+
+}
---
+++ src/test/java/ultra/hai12.java
@@ -0,0 +1,19 @@
+package ultra;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class hai12 {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
